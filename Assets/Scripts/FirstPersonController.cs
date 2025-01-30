467e03b132ef27297457f4e130222495dff41792
DESKTOP-GV16N45\Alumno <informatica@esne.es>
2025-01-30 21:19:52 +0100

    1

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,148 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FirstPersonController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private float gravityScale;
+
+    [SerializeField]
+    private float jumpHeight;
+
+    [SerializeField]
+    private float originalScale;
+
+    [SerializeField]
+    private float scaleWhenCrouched;
+
+    [SerializeField]
+    private float crouchedSpeed;
+
+    [Header("Ground Detection")]
+    [SerializeField]
+    private Transform feet;
+
+    [SerializeField]
+    private float detectionRadius;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    private Vector3 verticalMovement;
+    private CharacterController controller;
+    private Camera cam;
+
+    private PlayerInput playerinput;
+    private Vector2 input;
+
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        cam = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
+        playerinput = GetComponent<PlayerInput>();
+    }
+    private void OnEnable()
+    {
+        playerinput.actions["Jump"].started += Saltar;
+        playerinput.actions["Move"].performed += Movement;
+        playerinput.actions["Move"].canceled += CancelarMover;
+
+       //    playerinput.SwitchCurrentActionMap("Coche"); //solo te interpretaria esos inputs relacionados con ese parametro
+        //playerinput.deviceLostEvent.AddListener((x) => Debug.Log("Reconecta el mando por favor"));
+    }
+    private void Movement(InputAction.CallbackContext ctx)
+    {
+         input = ctx.ReadValue<Vector2>();
+    }
+
+    private void CancelarMover(InputAction.CallbackContext ctx)
+    {
+        input = new Vector2 (0,0);  //se cancela el input
+    }
+
+
+    private void Saltar(InputAction.CallbackContext ctx)
+    {
+      
+            if (IsGrounded())
+            {
+                verticalMovement.y = 0;
+                verticalMovement.y = Mathf.Sqrt(-2 * gravityScale * jumpHeight);
+            }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveAndRotate();
+        ApplyGravity();
+        Crouch();
+       
+
+    }
+
+    private void Crouch()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            transform.localScale /= 2; //A la mitad TODO para que no deforme objetos.
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            transform.localScale *= 2;
+        }
+    }
+
+    private void MoveAndRotate()
+    {
+
+
+        //Se aplica al cuerpo la rotación que tenga la cámara.
+        transform.rotation = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0);
+
+        ////Si hay input...
+        if (input.sqrMagnitude > 0)
+        {
+            //Se calcula el ángulo en base a los inputs
+            float angleToRotate = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+
+            //Se rota el vector (0, 0, 1) a dicho ángulo
+            Vector3 movementInput = Quaternion.Euler(0, angleToRotate, 0) * Vector3.forward;
+
+            //Se aplica movimiento en dicha dirección.
+            controller.Move(movementInput * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    
+
+    private void ApplyGravity()
+    {
+        verticalMovement.y += gravityScale * Time.deltaTime;
+        controller.Move(verticalMovement * Time.deltaTime);
+    }
+    private bool IsGrounded()
+    {
+        return Physics.CheckSphere(feet.position, detectionRadius, whatIsGround);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(feet.position, detectionRadius);
+    }
+}
